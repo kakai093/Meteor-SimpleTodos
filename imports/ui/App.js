8ae2903246c8b3d2ef7b009eed3d56c9b14641fa
Kassandra Cabaang <Kassandra Cabaang>
2017-03-31 01:28:12 +0800

    Display input field to user only and Shoe who user created the task

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDom from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -26,6 +27,8 @@ class App extends Component {
     Tasks.insert({
       text,
       createdAt: new Date(),
+      owner: Meteor.userId(),              //_id of Logged in user
+      username: Meteor.user().username,   // username of Logged in user
     });
 
     // Clear Form
@@ -65,14 +68,16 @@ class App extends Component {
         </label>
 
         <AccountsUIWrapper />
-      
+
+      { this.props.currentUser ?
         <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
               type="text"
               ref="textInput"
               placeholder="Type to add new tasks"
             />
-        </form>
+        </form> : ''
+      }
         </header>
 
         <ul>
@@ -86,11 +91,13 @@ class App extends Component {
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
   incompleteCount: PropTypes.number.isRequired,
+  currentUser: PropTypes.object,
 }  ;
 
 export default createContainer (() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(), // Sort from newest Post to Oldest
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
+    currentUser: Meteor.user(),
   };
 }, App);
